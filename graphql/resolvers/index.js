7604855b920cec94dba8b45096976ecc5a4faf7f
Kimleang <kimleang.hrd@gmail.com>
2019-11-04 19:59:05 +0700

    Add Booking Event

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs')
 const Event = require('../../models/event')
 const User = require('../../models/user')
+const Booking = require('../../models/booking')
 
 const events = async eventIds => {
     try {
@@ -17,6 +18,19 @@ const events = async eventIds => {
                 creator: user.bind(this, event.creator)
             }
         })
+    } catch (err) {
+        throw err
+    }
+}
+
+const singleEvent = async eventId => {
+    try {
+        const event = await Event.findById(eventId)
+        return {
+            ...event._doc,
+            _id: event.id,
+            creator: user.bind(this, event._doc.creator)
+        }
     } catch (err) {
         throw err
     }
@@ -50,13 +64,30 @@ module.exports = {
             throw err
         }
     },
+    bookings: async () => {
+        try {
+            const bookings = await Booking.find()
+            return bookings.map(booking => {
+                return {
+                    ...booking._doc,
+                    _id: booking.id,
+                    user: user.bind(this, booking._doc.user),
+                    event: singleEvent.bind(this, booking._doc.event),
+                    createdAt: new Date(booking._doc.createdAt).toISOString(),
+                    updatedAt: new Date(booking._doc.updatedAt).toISOString()
+                }
+            })
+        } catch (err) {
+            throw err
+        }
+    },
     createEvent: async (args) => {
         const event = new Event({
             title: args.event.title,
             description: args.event.description,
             price: +args.event.price,
             date: new Date(args.event.date),
-            creator: '5dbedfc1d0c3042640efda9a'
+            creator: '5dc019dc75b1cb0adee22641'
         })
         try {
             const result = await event.save()
@@ -66,7 +97,7 @@ module.exports = {
                 date: new Date(result._doc.date).toISOString(),
                 creator: user.bind(this, result._doc.creator)
             }
-            const creator = await User.findById('5dbedfc1d0c3042640efda9a')
+            const creator = await User.findById('5dc019dc75b1cb0adee22641')
             if (!creator) {
                 throw new Error('User not found')
             }
@@ -100,6 +131,42 @@ module.exports = {
                 password: null,
                 _id: userSaveResult._id
             }
+        } catch (err) {
+            throw err
+        }
+    },
+    bookEvent: async (args) => {
+        try {
+            const fetchedEvent = await Event.findOne({
+                _id: args.eventId
+            })
+            const booking = new Booking({
+                user: '5dc019dc75b1cb0adee22641',
+                event: fetchedEvent
+            })
+            const result = await booking.save()
+            return {
+                ...result._doc,
+                _id: result.id,
+                user: user.bind(this, booking._doc.user),
+                event: singleEvent.bind(this, booking._doc.event),
+                createdAt: new Date(result._doc.createdAt).toISOString(),
+                updatedAt: new Date(result._doc.updatedAt).toISOString()
+            }
+        } catch (err) {
+            throw err
+        }
+    },
+    cancelBooking: async (args) => {
+        try {
+            const booking = await Booking.findById(args.bookingId).populate('event');
+            const event = {
+                ...booking.event._doc,
+                _id: booking.event.id,
+                creator: user.bind(this, booking.event._doc.creator)
+            }
+            await Booking.deleteOne({ _id: args.bookingId })
+            return event
         } catch (err) {
             throw err
         }
